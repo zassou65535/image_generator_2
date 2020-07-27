ac89d3e501c380c5d43c67a8f174d601b2a04227
zassou65535 <gravity@kuf.biglobe.ne.jp>
2020-07-27 23:32:54 +0900

    学習に関する箇所に説明を追加

--- GAN.py
+++ GAN.py
@@ -4,8 +4,12 @@ from module.importer import *
 from module.base_module import *
 from module.discriminator import *
 from module.generator import *
+from module.dataloader import *
 
-#学習と誤差伝搬を行う関数
+#discriminatorの損失関数の、勾配制約項の計算に必要な関数
+#損失関数にはWGAN-GPを用いる
+#discriminatorでは、E[本物の画像の判定結果]-E[偽画像の判定結果]+勾配制約項　と表され、
+#generatorでは、E[偽画像の判定結果]と表される
 def gradient_penalty(netD, real, fake, res, batch_size, gamma=1):
 	device = real.device
 	#requires_gradが有効なTensorに対してはbackwardメソッドが呼べて、自動的に微分を計算できる
@@ -30,18 +34,28 @@ f __name__ == '__main__':
 	#generator,discriminatorの誤差伝搬の最適化手法にはAdamを指定
 	optG = torch.optim.Adam(netG.parameters(), lr=0.0005, betas=(0.0, 0.99))
 	optD = torch.optim.Adam(netD.parameters(), lr=0.0005, betas=(0.0, 0.99))
+	#誤差関数の定義
 	criterion = torch.nn.BCELoss()
 
 	# dataset
-	transform = transforms.Compose([transforms.CenterCrop(160),
-	                                transforms.Resize((128,128)),
-	                                transforms.ToTensor(), ])
+	# transform = transforms.Compose([transforms.CenterCrop(160),
+	#                                 transforms.Resize((128,128)),
+	#                                 transforms.ToTensor(), ])
 
-	trainset = datasets.CelebA('~/data', download=True, split='train',
-	                           transform=transform)
+	# trainset = datasets.CelebA('~/data', download=True, split='train',
+	#                            transform=transform)
 
-	bs = 8
-	train_loader = DataLoader(trainset, batch_size=bs, shuffle=True)
+	# bs = 8
+	# train_loader = DataLoader(trainset, batch_size=bs, shuffle=True)
+
+	#訓練データの読み込み、データセット作成
+	train_img_list = make_datapath_list()
+	mean = (0.5,)
+	std = (0.5,)
+	train_dataset = GAN_Img_Dataset(file_list=train_img_list,transform=ImageTransform(mean,std,resize_width_height_pixel=128))
+	#データローダー作成
+	batch_size = 8#ミニバッチあたりのサイズ
+	train_dataloader = torch.utils.data.DataLoader(train_dataset,batch_size=batch_size,shuffle=True)
 
 	#学習開始
 	#エポック数
@@ -58,23 +72,33 @@ f __name__ == '__main__':
 			optG.param_groups[0]['lr'] = 0.0001
 			optD.param_groups[0]['lr'] = 0.0001
 
-		for i, data in enumerate(train_loader):
-			x, y = data
+		#for i, data in enumerate(train_loader):
+		#データローダーからminibatchずつ取り出す
+		for imgs in train_dataloader:
+			x = imgs
+			#GPUが使えるならGPUへ転送
 			x = x.to(device)
 			res = j/res_step
 
-			#generaorの学習
+			#----------generaorの学習----------
 			#ノイズを生成
 			z = torch.randn(bs, 512*16).to(x.device)
 			#ノイズをgeneratorに入力、出力画像をx_とする
 			x_ = netG.forward(z, res)
 			#出力画像x_をdiscriminatorで推論　つまり偽画像の入力をする
 			d_ = netD.forward(x_, res)
-			# WGAN_GPではミニバッチ内の推論結果全てに対し平均を取り、それを誤差伝搬に使う
-			lossG = -d_.mean()
+			#損失関数にはWGAN-GPを用いる
+			#discriminatorでは、E[本物の画像の判定結果]-E[偽画像の判定結果]+勾配制約項　と表され、
+			#generatorでは、E[偽画像の判定結果]と表される
 
+			# WGAN_GPではミニバッチ内の推論結果全てに対し平均を取り、それを誤差伝搬に使う
+			lossG = -d_.mean()#E[偽画像の判定結果]を計算
+
+			#前のイテレーションで計算した傾きが残ってしまっているのでそれをリセットしておく
 			optG.zero_grad()
+			#損失の傾きを計算して
 			lossG.backward()
+			#実際に誤差伝搬を行う
 			optG.step()
 
 			# update netG_mavg by moving average
@@ -83,27 +107,42 @@ f __name__ == '__main__':
 			for p_mavg, p in zip(netG_mavg.parameters(), netG.parameters()):
 				p_mavg.data = alpha*p_mavg.data + (1.0-alpha)*p.data
 
-			#discriminatorの学習
+			#----------discriminatorの学習----------
+			#ノイズを生成、zとする
 			z = torch.randn(x.shape[0], 512*16).to(x.device)
+			#generatorにノイズを入れ偽画像を生成、x_とする
 			x_ = netG.forward(z, res)
+			#平均を取ることでxの次元を変換
 			x = F.adaptive_avg_pool2d(x, x_.shape[2:4])
-			d = netD.forward(x, res)   # real
-			d_ = netD.forward(x_, res) # fake
-			loss_real = -d.mean()
-			loss_fake = d_.mean()
+			#本物の画像を判定、結果をdに格納
+			d = netD.forward(x, res)
+			#偽画像を判定、結果をd_に格納
+			d_ = netD.forward(x_, res)
+
+			#損失関数にはWGAN-GPを用いる
+			#discriminatorでは、E[本物の画像の判定結果]-E[偽画像の判定結果]+勾配制約項　と表され、
+			#generatorでは、E[偽画像の判定結果]と表される
+
+			#ミニバッチごとの、判定結果の平均をそれぞれとる
+			loss_real = -d.mean()#E[本物の画像の判定結果]を計算
+			loss_fake = d_.mean()#-E[偽画像の判定結果]を計算
+			#勾配制約項の計算
 			loss_gp = gradient_penalty(netD, x.data, x_.data, res, x.shape[0])
 			loss_drift = (d**2).mean()
-
 			beta_gp = 10.0
 			beta_drift = 0.001
+			#E[本物の画像の判定結果]-E[偽画像の判定結果]+勾配制約項 を計算
 			lossD = loss_real + loss_fake + beta_gp*loss_gp + beta_drift*loss_drift
 
+			#前のイテレーションで計算した傾きが残ってしまっているのでそれをリセットしておく
 			optD.zero_grad()
+			#損失の傾きを計算して
 			lossD.backward()
+			#実際に誤差伝搬を行う
 			optD.step()
 
-			print('ep: %02d %04d %04d lossG=%.10f lossD=%.10f' %
-				(iepoch, i, j, lossG.item(), lossD.item()))
+			print('ep: %02d %04d lossG=%.10f lossD=%.10f' %
+				(iepoch, j, lossG.item(), lossD.item()))
 
 			losses.append([lossG.item(), lossD.item()])
 			j += 1
---
+++ module/dataloader.py
@@ -0,0 +1,62 @@
+#encoding:utf-8
+
+from .importer import *
+import os.path as osp
+
+def make_datapath_list():
+	target_path = "./*/*"
+	path_list = []#画像ファイルパスのリストを作り、戻り値とする
+	for path in glob.glob(target_path):
+		path_list.append(path)
+		print(path)
+	print(str(len(path_list)))
+	return path_list
+
+class ImageTransform():
+	#画像の前処理クラス
+	def __init__(self,mean,std,resize_width_height_pixel):
+		self.data_transform = transforms.Compose([
+				transforms.Resize(resize_width_height_pixel),
+				transforms.ToTensor(),
+				transforms.Normalize(mean,std)
+			])
+	def __call__(self,img):
+		return self.data_transform(img)
+
+class GAN_Img_Dataset(data.Dataset):
+	#画像のデータセットクラス
+	def __init__(self,file_list,transform):
+		self.file_list = file_list
+		self.transform = transform
+	#画像の枚数を返す
+	def __len__(self):
+		return len(self.file_list)
+	#前処理済み画像の、Tensor形式のデータを取得
+	def __getitem__(self,index):
+		img_path = self.file_list[index]
+		img = Image.open(img_path)#[RGB][高さ][幅]
+		img_transformed = self.transform(img)
+		return img_transformed
+
+# #動作確認
+# train_img_list = make_datapath_list()
+
+# mean = (0.5,)
+# std = (0.5,)
+# train_dataset = GAN_Img_Dataset(file_list=train_img_list,transform=ImageTransform(mean,std,resize_width_height_pixel=64))
+
+# batch_size = 1
+# train_dataloader = torch.utils.data.DataLoader(train_dataset,batch_size=batch_size,shuffle=True)
+
+# batch_iterator = iter(train_dataloader)
+# imgs = next(batch_iterator)
+# print(imgs.size())
+
+# fig = plt.figure()
+# img_transformed = imgs[0].detach().numpy().transpose(1,2,0)
+# plt.imshow(img_transformed)
+# fig.savefig("img/img.png")
+
+
+
+
