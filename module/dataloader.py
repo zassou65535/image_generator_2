c831646ae04347f6c2fbb4fbbb952d87714dcc2b
zassou65535 <gravity@kuf.biglobe.ne.jp>
2020-07-30 00:30:54 +0900

    細かい箇所を修正

--- module/dataloader.py
+++ module/dataloader.py
@@ -43,12 +43,12 @@ class GAN_Img_Dataset(data.Dataset):
 		img_transformed = self.transform(img)
 		return img_transformed
 
-# #動作確認
+#動作確認
 # train_img_list = make_datapath_list()
 
 # mean = (0.5,)
 # std = (0.5,)
-# train_dataset = GAN_Img_Dataset(file_list=train_img_list,transform=ImageTransform(mean,std,resize_width_height_pixel=64))
+# train_dataset = GAN_Img_Dataset(file_list=train_img_list,transform=ImageTransform(mean,std,resize_width_height_pixel=256))
 
 # batch_size = 1
 # train_dataloader = torch.utils.data.DataLoader(train_dataset,batch_size=batch_size,shuffle=True)
